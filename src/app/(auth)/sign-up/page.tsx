8457adf240a32da277149ebf030531475fc94b91
divya562 <divyabagul2502@gmail.com>
2024-05-27 18:36:58 +0530

    react-hook-form/ shadcn / debouncing

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,163 @@
+"use client"
+import { useForm } from "react-hook-form"
+import { Form } from "@/components/ui/form";
+import * as z from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceCallback} from 'usehooks-ts';
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+import { signUpValidation } from "@/schemas/signupSchema"
+import axios, {AxiosError} from 'axios';
+import { ApiResponse } from "@/types/ApiResponse"
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { FormProvider } from 'react-hook-form';
+
+
+const page=()=> {
+    
+  const [username, setUsername] = useState('')
+  const[usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const debounced = useDebounceCallback(setUsername, 300)
+  const {toast} = useToast()
+  const router = useRouter()
+
+  //zod implementation
+  const form = useForm({
+    resolver: zodResolver(signUpValidation),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: ''
+    }
+  })
+
+  useEffect(()=>{
+    const checkUserNameUnique = async() =>{
+      if(username){
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+
+        try {
+         const resp =  await axios.get(`/api/check-username?username=${username}`)
+          setUsernameMessage(resp.data.message)
+        } catch (error) {
+          const AxiosError = error as AxiosError<ApiResponse>
+          setUsernameMessage(
+            AxiosError.response?.data.message ?? "Error checking Username"
+          )
+        }
+        finally{
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUserNameUnique()
+  },[username])
+
+  const onSubmit = async(data: z.infer<typeof signUpValidation>) =>{
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post('/api/sign-up', data)
+      toast({
+        title:"success",
+        description: response.data.message
+      })
+      router.replace('/verify/${username}')
+      setIsSubmitting(false)
+    } catch (error) {
+      console.error("Error in Sign up of user", error)
+      const AxiosError = error as AxiosError<ApiResponse>
+      let ErrorMessage = AxiosError.response?.data.message
+      toast({
+        title:"Signup Failed",
+        description: ErrorMessage,
+        variant: "destructive"
+      })
+      setIsSubmitting(false)
+    }
+  }
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6"> Join Mystery Message</h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+      </div>
+      <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField 
+            control={form.control}
+            name="username"
+            render={({field})=>(
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field}
+                  onChange={
+                    (e)=>{
+                      field.onChange(e) 
+                      debounced(e.target.value)
+                    }}
+                />
+              </FormControl>
+              {isCheckingUsername && <Loader2 className="animate-spin"/>}
+                    <p className={`text-sm ${usernameMessage === "Username is Available" ? 'text-green-500' : 'text-red-500'}`}>Test {usernameMessage}</p>
+              <FormMessage/>
+            </FormItem>
+            )}/>
+            <FormField 
+            control={form.control}
+            name="email"
+            render={({field})=>(
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+            )}/>
+            <FormField 
+            control={form.control}
+            name="password"
+            render={({field})=>(
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+            )}/>
+            <Button type="submit" aria-disabled={isSubmitting}>
+              {
+               
+                isSubmitting ? (
+                  <>
+                  <Loader2  className="mr-2 h-4 w-4 animate-spin"/> Please wait 
+                </>
+                ) : ('Signup')
+              }
+            </Button>
+          </form>
+      </Form>
+      <div>
+        <p className="text-center mt-4">
+          Already a member ? {' '}
+          <Link href='/sign-in' className="text-blue-600 hover:text-blue-800">Sign in</Link>
+        </p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default page
--- src/schemas/signupSchema.ts
+++ src/schemas/signupSchema.ts
@@ -4,10 +4,10 @@ export const usernameValidation = z
     .string()
     .min(2, "Username must be atleast 2 characters")
     .max(20, "Username must be not more than 20 characters")
-    .regex(/^[a-zA-Z0-9_]+$/,"Username must not contain special charactes")
+    .regex(/^(?! )[a-zA-Z0-9_ ]*(?<! )$/,"Username must not contain special charactes")
 
 export const signUpValidation= z.object({
     username: usernameValidation,
     email: z.string().email({message: 'Invalid Email Address'}),
-    password: z.string().min(8, {message: 'Password must be atleast 6 character'})
+    password: z.string().min(8, {message: 'Password must be atleast 8 character'})
 })
