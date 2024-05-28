d33e3b82eeb96df5f061021c5359442251180430
divya562 <divyabagul2502@gmail.com>
2024-05-28 16:28:49 +0530

    dashboard page

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,10 +17,10 @@ export async function middleware(request: NextRequest){
     ){
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    if(!token && url.pathname.startsWith('/dashboard')){
-             return NextResponse.redirect(new URL('/sign-in', request.url))
+    // if(!token && url.pathname.startsWith('/dashboard')){
+    //          return NextResponse.redirect(new URL('/sign-in', request.url))
    
-    }
+    // }
     // return NextResponse.redirect(new URL('/home', request.url))
 
 }
